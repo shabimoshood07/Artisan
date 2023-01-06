d5dfb56564d895471dab578b2e51eb9cf52fe9ef
Moshood Olawale <shabimoshood07@gmail.com>
2023-01-06 23:09:56 +0100

    WelcomeJs and styles

--- src/features/Welcome/Welcome.js
+++ src/features/Welcome/Welcome.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // MATERIAL UI IMPORT
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
 
 // CSS IMPORT
 import "./style.css";
@@ -9,7 +9,9 @@ import "./style.css";
 const Welcome = () => {
   return (
     <Container className="main-con">
-      <Box sx={{ border: "solid", height: "100%" }}>
+      <Box sx={{ border: "solid", height: "100%",
+       background: "rgba(0,0,0,.6)"
+        }}>
         <Grid
           direction={{ xs: "column", md: "row" }}
           container
@@ -19,20 +21,23 @@ const Welcome = () => {
             border: "solid blue",
             margin: "0",
             width: "100%",
+            alignItems: "center",
+            justifyContent: "center",
             // height: "100%",
           }}
         >
           <Grid md={8} sx={{ border: "solid yellow", height: "100%", p: 4 }}>
             <Typography className="welcome-text" align="left">
-              "Welcome to <span>Artisans! </span> We provide access to a team of
+              Welcome to <span>Artisans! </span> We provide access to a team of
               dedicated professionals committed to providing top-quality
               products and services to customers. We hope you find what you're
               looking for,and if you have any questions, don't hesitate to
-              contact us."
+              contact us
             </Typography>
           </Grid>
           <Grid md={8} sx={{ border: "solid green" }}>
-            <Typography>buttons</Typography>
+            <Button>Login</Button>
+            <Button>Sign up</Button>
           </Grid>
         </Grid>
       </Box>
